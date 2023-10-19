52775349da1a9af3d671e125ad56bd512f854a51
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-19 14:38:12 -0400

    attempting to work on local storage

--- to-do-list/app/page.js
+++ to-do-list/app/page.js
@@ -1,17 +1,28 @@
 "use client"
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 //import Image from 'next/image'
 //import styles from './page.module.css'
-import { TextField } from '@mui/material'
+import { Container, TextField } from '@mui/material'
 
 
 export default function Home() {
+
+  const [items, setItems] = React.useState
+
+  useEffect(() => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    if (items) {
+      setItems(items);
+    }
+  }, []);
+
+
   return (
     <main>
       <h1>TO DO:</h1>
-      <div>
-        <TextField id="outlined secondary" label="To Do" color="secondary" focused />
-      </div>
+      <Container>
+        <TextField id="outlined secondary" fullWidth label ="fullWidth" label="To Do" color="secondary" focused />
+      </Container>
     </main>
   )
 }
