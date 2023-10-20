764dfe345289b4ac13cdc3dd489eae7677d9f597
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-10-20 07:53:19 -0400

    rearranging, naming, and trying to add event handlers - everything is broken

--- to-do-list/app/components/input-field.js
+++ to-do-list/app/components/input-field.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextField } from '@mui/material';
 
-const ToDoInput = () => {
+const ToDoInput = ({ onAddTodo }) => {
+    const [todo, setTodo] = useState("");
+
+    const handleAddTodo = () => {
+        if (todo.trim()) {
+            onAddTodo()
+        }
+        const newTodo = event.target.value;
+        setTodo(newTodo);
+        //
+
+    }
+
+
 
     return (
-        <TextField>
-
-        </TextField>
+        <TextField type="text" onChange={(e) => setTodo(e.target.value)} value={todo}/>
     )
 }
